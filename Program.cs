34567dd1893a0279d631bf0390d6574bdcf1801d
Upendra <eupendra@gmail.com>
2021-07-21 12:32:28 +0530

    updates

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using HtmlAgilityPack;
+namespace CODE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var html = @"http://html-agility-pack.net/";
+            HtmlWeb web = new HtmlWeb();
+            var htmlDoc = web.Load(html);
+            var node = htmlDoc.DocumentNode.SelectSingleNode("//head/title");
+            Console.WriteLine("Node Name: " + node.Name + "\n" + node.OuterHtml);
+
+        }
+    }
+}
